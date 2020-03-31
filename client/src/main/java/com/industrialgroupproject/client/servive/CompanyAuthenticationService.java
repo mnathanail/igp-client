d37c4fd1e382bfe6e868e44a9a9f87d7eccbb042
Athanasios Tsagris <thanostsg@gmail.com>
2020-03-31 04:12:57 +0300

    Update CompanyAuthenticationService.java

--- client/src/main/java/com/industrialgroupproject/client/servive/CompanyAuthenticationService.java
+++ client/src/main/java/com/industrialgroupproject/client/servive/CompanyAuthenticationService.java
@@ -1,7 +1,13 @@
 package com.industrialgroupproject.client.servive;
 
 import com.industrialgroupproject.client.model.CompanyAuthenticationModel;
+import com.industrialgroupproject.client.model.CompanyModel;
 
 public interface CompanyAuthenticationService {
 	CompanyAuthenticationModel findByUserName(String userName);
+	
+	/*==== MINOR ====*/
+	String login(CompanyModel model);
+	/*==== MINOR ====*/
+	
 }
